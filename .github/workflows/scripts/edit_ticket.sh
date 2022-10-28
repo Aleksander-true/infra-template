8a5926127209cc85298050734edfff0a8550c095
Aleksander-true <Aleksander-true@yabdex.ru>
2022-10-28 12:51:28 +0300

    script 5

--- .github/workflows/scripts/edit_ticket.sh
+++ .github/workflows/scripts/edit_ticket.sh
@@ -1,28 +1,18 @@
 
 #!/bin/bash
-echo 'Start script'
+echo 'Get token to patch ticket'
 
 HEADER="Content-Type: application/json"
 URL="https://iam.api.cloud.yandex.net/iam/v1/tokens"
 
-echo "YA_TOKEN $YA_TOKEN"
-echo "COMPANY_ID $COMPANY_ID"
-echo "TICKET_TEXT $TICKET_TEXT"
-
 RESULT=$(curl -d "{'yandexPassportOauthToken': '$YA_TOKEN' }" -H $HEADER -X POST $URL)
-echo "Rezult = $RESULT"
-
 IAMTOKEN=$(echo $RESULT | grep -oP '(?<=: )(.+)(?=,)' | tr -d '"')
 
-echo "IAMTOKEN $IAMTOKEN"
+echo "Patch ticket description"
 
 AUTHHEADER="Authorization: Bearer $IAMTOKEN"
 IDHEADER="X-Org-ID: $COMPANY_ID"
 TICKET="HOMEWORKSHRI-140"
-
-echo "IDHEADER $IDHEADER"
-
-#TICKET_TEXT="Заголовок Заголовок Заголовок 12344"
 
 TEXT=$(curl --write-out %{http_code} -H "$AUTHHEADER" -H "$IDHEADER" -d "{\"description\": \"$TICKET_TEXT\"}" -X PATCH "https://api.tracker.yandex.net/v2/issues/$TICKET" )
 
