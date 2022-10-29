1b8e4ca1b934affe94bcf217a995f6196bd64af8
Aleksander-true <Aleksander-true@yabdex.ru>
2022-10-29 20:22:31 +0300

    script

--- .github/workflows/scripts/edit_ticket.sh
+++ .github/workflows/scripts/edit_ticket.sh
@@ -1,11 +1,19 @@
 
 #!/bin/bash
-LATEST_TAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1))
-PREVIOUS_TAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))
+LATEST_TAG=$(git describe --match "rc-*" --abbrev=0 --tags $(git rev-list --tags --max-count=1))
+PREVIOUS_TAG=$(git describe --match "rc-*" --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))
 
-COMMITS_BETWEN=$(git log --pretty=format:"%h  %cn  %s %d \n" $PREVIOUS_TAG..$LATEST_TAG | grep -v tag)
+if [ -z "$PREVIOUS_TAG" ]
+then 
+	COMMITS_BETWEN=$(git log --pretty=format:"%h  %cn  %s %d \n" $LATEST_TAG | grep -v tag)
+else 
+	COMMITS_BETWEN=$(git log --pretty=format:"%h  %cn  %s %d \n" $PREVIOUS_TAG..$LATEST_TAG | grep -v tag)
+fi
 
 TICKET_TEXT="Responsible for release $GITHUB_ACTOR \n commits included in the release: \n$(echo $COMMITS_BETWEN)"
+
+echo "COMMITS_BETWEN $COMMITS_BETWEN"
+echo "TICKET_TEXT $TICKET_TEXT"
 
 echo 'Get token to patch ticket'
 
